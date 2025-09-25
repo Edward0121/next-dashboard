fd6e91079797c2417ef8294f01f814a90806c423
liushuait <liushauit@yonyou.com>
2025-09-25 09:43:41 +0800

    daily: 修改字库子集

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,8 @@
 import { Inter, Lusitana } from "next/font/google";
+
 export const inter = Inter({ subsets: ["latin"] });
 
 export const lusitana = Lusitana({
-  subsets: ["Lusitana"],
+  subsets: ["latin"], // 修正：Lusitana 字体只支持 latin 子集
   weight: ["400", "700"],
 });
